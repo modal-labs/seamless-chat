50c04878f2ce12ee2398e8f827975c3cef225ec7
Vishaal Ram <vishaalram02@gmail.com>
2024-10-10 16:36:43 -0400

    setup proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://modal-labs-vishy-dev--seamless-chat-seamlessm4t-asgi-app.modal.run',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
 });
